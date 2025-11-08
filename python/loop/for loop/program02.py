67b4e4fe97e25f0842fcf9ffcce3fc54d4fb91b0
Daleepswami <daleepswami22170@gmail.com>
2025-11-08 23:07:59 +0530

    Update program02.py

--- python/loop/for loop/program02.py
+++ python/loop/for loop/program02.py
@@ -1,1 +1,12 @@
-program02
+#Write a program to print all Prime numbers between give range
+print("please Enter the renge of two number for finding prime number :-" )
+num1=int(input("Enter number their start the range :-"))
+num2=int(input("Enter number their end the range :-"))
+for i in range(num1,num2):
+    count=0
+    for j in range(1,i+1):
+       if i%j==0:
+           count+=1
+    if count==2:
+        print(i)
+         
